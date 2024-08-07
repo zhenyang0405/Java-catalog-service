ea854c5e3fbf54b974b2b0756641962f3ac5a5b7
Zhen Yang <zhenyang.chong0405@gmail.com>
2024-08-07 22:48:55 +0800

    v4-Spring Cloud Config

--- src/main/java/com/polarbookshop/catalog_service/HomeController.java
+++ src/main/java/com/polarbookshop/catalog_service/HomeController.java
@@ -1,13 +1,19 @@
 package com.polarbookshop.catalog_service;
 
+import com.polarbookshop.catalog_service.config.PolarProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
+    private final PolarProperties polarProperties;
+
+    public HomeController(PolarProperties polarProperties) {
+        this.polarProperties = polarProperties;
+    }
 
     @GetMapping("/")
     public String getGreeting() {
-        return "Welcome to the book catalog!";
+        return polarProperties.getGreeting();
     }
 }
